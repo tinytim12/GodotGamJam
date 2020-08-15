3e3f389e76a1b925ccc9f005dc3f52a98822eb7f
Paruthi <Paruthidotexe@gmail.com>
2020-08-16 02:54:50 +0530

    Temporary player

---
+++ Src/Scripts/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var speed = 64
+var velocity = Vector2.ZERO
+var jump_speed = -128
+var gravity = 10
+var ground_normal = Vector2(0, -1)
+
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	else:
+		velocity.x = 0
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = jump_speed
+
+	velocity.y += gravity
+	
+	velocity = move_and_slide(velocity, ground_normal)
