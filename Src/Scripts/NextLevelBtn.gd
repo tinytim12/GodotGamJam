b1a3123ee4b3657e29fbbc539c822f89c4e645e2
Paruthi <Paruthidotexe@gmail.com>
2020-08-16 13:19:47 +0530

    Three levels added

---
+++ Src/Scripts/NextLevelBtn.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+#export (PackedScene) var next_level
+export (String, FILE) var scene_path
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_NextLevelBtn_up():	
+	get_tree().change_scene(scene_path)
