2d7592ff83cbb1ae7cedca542f4a844d109464c9
Paruthi <Paruthidotexe@gmail.com>
2020-08-16 14:41:43 +0530

    updated player + other pngs

--- Src/Scripts/Player.gd
+++ Src/Scripts/Player.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 var speed = 64
 var velocity = Vector2.ZERO
-var jump_speed = -128
-var gravity = 10
+var jump_speed = -512
+var gravity = 32
 var ground_normal = Vector2(0, -1)
 
 func _ready():
@@ -17,7 +17,7 @@ func _physics_process(delta):
 		velocity.x = speed
 	else:
 		velocity.x = 0
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_speed
 
 	velocity.y += gravity
