0bda02c74948906c57a652de451caa8cd23d86d1
Paruthi <Paruthidotexe@gmail.com>
2020-08-18 05:30:29 +0530

    Merged the Light effects to player
    added design images

--- Src/Scripts/Player.gd
+++ Src/Scripts/Player.gd
@@ -23,43 +23,82 @@ onready var player_camera = get_node("MainCamera")
 var tilemap_rect
 var tilemap_cell_size
 
+# ------------------------------------------------------------------------------
+# Kid reddening effect
+# ------------------------------------------------------------------------------
+export(NodePath) var parentP
+export(NodePath) var redP
+export(int) var threshold
+var dangerDistance = 15
+var reddenRate = 0.002
+var lightRate = 30
+var cameraThreshold = 35
+var _timer = null
+
+onready var red = get_node(redP)
+onready var parent = get_node(parentP)
+onready var camera = parent.get_node("MainCamera")
+onready var light = get_node("KidEffects/Light2D")
+# ------------------------------------------------------------------------------
+
 func _ready():
 	Engine.set_target_fps(TARGET_FPS)
 	if is_kid == true:
 		GRAVITY = -GRAVITY
-		SPEED = SPEED
+		SPEED = SPEED - childSpeedModifier
 		JUMP_SPEED = -JUMP_SPEED
 		ground_normal = -ground_normal
 		player_sprite.texture = char_texture
 		player_camera.current = false
 		player_sprite.flip_v = true
-
-	# camera settings
-	tilemap_rect = get_parent().get_node("TileMap").get_used_rect()
-	tilemap_cell_size = get_parent().get_node("TileMap").cell_size
-	player_camera.limit_right = tilemap_rect.end.x * tilemap_cell_size.x
+# ------------------------------------------------------------------------------
+		light.set("energy", 0.0)
+		threshold = 50
+		
+		_timer = Timer.new()
+		add_child(_timer)
+	
+		_timer.connect("timeout", self, "_on_Timer_timeout")
+		_timer.set_wait_time(0.2)
+		_timer.set_one_shot(false) # Make sure it loops
+		_timer.start()
+		 
+		red = get_node(redP)
+		red.modulate.a = 0
+# ------------------------------------------------------------------------------
+		# remove camera
+		$MainCamera.queue_free()
+	else:
+		# parent remove the kid effects 
+		$KidEffects.queue_free()
+	
+	# camera settings for parent
+	if get_parent().get_node("TileMap") != null:
+		tilemap_rect = get_parent().get_node("TileMap").get_used_rect()
+		tilemap_cell_size = get_parent().get_node("TileMap").cell_size
+		player_camera.limit_right = tilemap_rect.end.x * tilemap_cell_size.x
 
 
 func _physics_process(delta):
 	# input handling
 	if Input.is_action_pressed("ui_left"):
 		if is_kid == true:
-			velocity.x = -SPEED + childSpeedModifier
+			velocity.x = -SPEED #+ childSpeedModifier
 		else:
 			velocity.x = -SPEED
 	elif Input.is_action_pressed("ui_right"):
 		if is_kid == true:
-			velocity.x = SPEED - childSpeedModifier
+			velocity.x = SPEED #- childSpeedModifier
 		else:
 			velocity.x = SPEED
 	elif is_kid == true:
 		var distance = get_node("/root/Level/Parent").position.x - position.x
 		if ( abs(distance) >1):
 			if (distance < 0):
-				velocity.x = -SPEED + childSpeedModifier
+				velocity.x = -SPEED #+ childSpeedModifier
 				#print("too far")
 			else:
-				velocity.x = SPEED - childSpeedModifier
+				velocity.x = SPEED #- childSpeedModifier
 		else: 
 			velocity.x = 0
 	else:
@@ -82,10 +121,46 @@ func _physics_process(delta):
 	# update the player art and animation
 	update_player()
 
+
 # update the player art and animation
 func update_player():
 	if(velocity.x < 0):
 		player_sprite.flip_h = true	
 	elif(velocity.x > 0):
 		player_sprite.flip_h = false
+
+
+# ------------------------------------------------------------------------------
+# kid reddening effect
+# ------------------------------------------------------------------------------
+func _on_Timer_timeout():
+	#print("Second!")
+	_checkDistance()
+
+
+func _checkDistance():
+	var distance = abs(position.x - parent.position.x)
+	if (distance > dangerDistance):
+		light.set("energy", distance / lightRate);
+		threshold -= 1;
+		if (red.modulate.a < 0.7):
+			red.modulate.a = reddenRate * distance
+			#print("red.modulate.a")
+		if (threshold < 0):
+			_gameOver()
+			
+		if (threshold < cameraThreshold):
+			camera.set_offset(Vector2( \
+		rand_range(-1.0, 1.0) * (cameraThreshold - threshold ) / 5, \
+		rand_range(-1.0, 1.0) * (cameraThreshold - threshold ) / 5 \
+	))
+	else:
+		threshold = 50
+		red.modulate.a = 0
+		if light != null:
+			light.set("energy", 0)
+
+
+func _gameOver():
+	get_tree().reload_current_scene()
 
