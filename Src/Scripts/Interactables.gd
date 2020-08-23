8be89f0d31fac4cf007e1bd32813438e39f1ac21
Paruthi <Paruthidotexe@gmail.com>
2020-08-23 23:52:08 +0530

    switches and chapter 03 optmized

--- Src/Scripts/Interactables.gd
+++ Src/Scripts/Interactables.gd
@@ -4,18 +4,27 @@ export (String, "Star", "Key", "Switch", "Door" ) var itype
 
 signal OnInteraction(val)
 
-# Called when the node enters the scene tree for the first time.
+var switch_off
+var switch_on
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	switch_off = get_node("Sprite")
+	switch_on = get_node("SpriteOn")
+	if switch_off != null and switch_on != null:
+		switch_off.show()
+		switch_on.hide()
 
 
 func _on_Player_entered(body):
+	var is_player = false
 	if (body.name == "Kid"):
 		emit_signal("OnInteraction", itype, "Kid")
+		is_player = true
 	elif (body.name == "Parent"):
+		is_player = true
 		emit_signal("OnInteraction", itype, "Parent")
+	
+	if itype == "Switch" and is_player:
+		if switch_off != null and switch_on != null:
+			switch_off.hide()
+			switch_on.show()
